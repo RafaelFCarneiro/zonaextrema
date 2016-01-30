41ca76a6f047c4940e4f4b2787ddbd592dec61ae
Christiano Milfont <cmilfont@gmail.com>
2016-01-30 16:39:08 -0300

    Nao esta funfando

--- api/services/indexer.js
+++ api/services/indexer.js
@@ -1,8 +1,19 @@
+//import * as Processors from "processors";
+
 class Indexer {
+
+    findProcessor( log ) {
+
+        this.log = log;
+        let className = this.log.kind.description;
+
+        return new Processors[className](log);
+
+    }
 
     createTags(log) {
 
-        let processor = Biohacking.Processors.find( log );
+        let processor = this.findProcessor( log );
 
         log.description.split(" ").forEach( tag => processor.execute(tag) );
 
---
+++ api/services/processors.js
@@ -0,0 +1,31 @@
+class Eat {
+
+    constructor(log) {
+        this.log = log;
+    }
+
+    execute(tag) {
+
+    }
+
+    getTags() {
+
+    }
+
+}
+
+class Drink {
+
+    constructor(log) {
+        this.log = log;
+    }
+
+    execute(tag) {
+
+    }
+
+    getTags() {
+
+    }
+
+}
