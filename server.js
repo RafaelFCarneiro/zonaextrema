bad2af940c41fa3da7c53832d5192efe84d6022f
Marcelo Silva <marcelosilva.developer@gmail.com>
2016-01-31 00:23:54 -0300

    Mapping default route and render sinmple main template app

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var client = new elasticsearch.Client({ host: 'localhost:9200',  log: 'trace' })
 
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'jade');
+
+require('./api/controllers/index.js')(client, app);
 require('./api/controllers/charts.js')(client, app);
 
 app.listen(8080);
