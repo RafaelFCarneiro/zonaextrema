d7399298e15cae60bdcf7692f10b7416327f773a
Marcelo Silva <marcelosilva.developer@gmail.com>
2016-01-31 10:30:49 -0300

    AWS Lambda configuration ok

---
+++ aws.js
@@ -0,0 +1,13 @@
+const AWS = require('aws-sdk');
+
+AWS.config.loadFromPath('./config_aws.json');
+
+let lambda = new AWS.Lambda({
+  role: 'lambda_basic_execution'
+});
+
+lambda.invoke({ FunctionName: 'ZonaExtremaFunction' }, function(err, data) {
+  if (err) console.log(err);
+
+  console.log(data);
+});
