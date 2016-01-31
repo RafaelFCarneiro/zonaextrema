6ecf303709123a24dc7ee0ccd13f6f4a0f04c56e
Marcelo Silva <marcelosilva.developer@gmail.com>
2016-01-31 08:39:42 -0300

    Add server script to package.json

--- biohacking.js
+++ biohacking.js
@@ -1,13 +1,13 @@
 import elasticsearch from 'elasticsearch';
 import express from 'express';
 
-var client = new elasticsearch.Client({ host: 'localhost:9200',  log: 'trace' });
+const client = new elasticsearch.Client({ host: 'localhost:9200',  log: 'trace' });
 
-var app = express();
+const app = express();
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'jade');
 
 require('./api/controllers/index.js')(client, app);
 require('./api/controllers/charts.js')(client, app);
 
-app.listen(8080);
+app.listen(process.env.PORT || 8080);
