2c42d3428acda7e46c492d9df33a10b131d1a730
Christiano Milfont <cmilfont@gmail.com>
2016-03-12 14:24:34 -0300

    Exemplo de componentização

--- views/index.jsx
+++ views/index.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import DefaultLayout from './default.jsx'
 
+class Username extends React.Component {
+    render() {
+
+        var content;
+        var loginOrLogout;
+        var user = this.props.user;
+        if (user) {
+            content = <div>{user.username}</div>;
+            loginOrLogout = <a href="/logout">Logout</a>;
+        } else {
+            loginOrLogout = <a href="/login">Login</a>
+        }
+
+        return (
+            <div>
+                
+                {content}
+                {loginOrLogout}
+            </div>
+        );
+    }
+};
 
 export default class Index extends React.Component {
     render() {
-
         return (
             <DefaultLayout title={this.props.title}>
-
-                <div>
-
-                    <a href="/login">Login</a>
-
-                </div>
-
+                <Username user={this.props.user}/>
             </DefaultLayout>
         );
     };
